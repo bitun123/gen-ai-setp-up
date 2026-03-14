7bae7b834b3dfa9a94d3d5fdb69cb8ca2f57508a
satyajit <satyajitdasmahapatra2002@gmail.com>
2026-03-14 14:02:29 +0530

    add email sending functionality with nodemailer and zod validation

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
 import "dotenv/config"
-import  readline from'readline/promises';
+import readline from "readline/promises";
 import { ChatMistralAI } from "@langchain/mistralai";
-import { HumanMessage } from "langchain";
+import { HumanMessage, tool, createAgent } from "langchain";
+import { sendEmail } from "./mail.service.js";
+import * as z from "zod";
+
+
+
+const emailTool = tool(
+    sendEmail,
+    {
+        name:"emailTool",
+        description:"A tool to send emails.",
+        schema:z.object({
+            to: z.string().describe("The recipient's email address"),
+            html:z.string().describe("The HTML content of the email"),
+            subject: z.string().describe("The subject of the email"),
+        })
+    }
+)
+
+
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 const model = new ChatMistralAI({
-model: "mistral-small-latest",
-temperature: 0
+  model: "mistral-small-latest",
+  temperature: 0,
 });
 
-const message = []
+const agent  = createAgent({
+    model,
+    tools:[emailTool]
+})
 
-while(true){
-const userInput  = await rl.question(`\x1b[32m You:${'\x1b[0m '}`);
+const messages = [];
 
-message.push(new HumanMessage(userInput));
+while (true) {
+    const userInput = await rl.question("\x1b[32mYou:\x1b[0m ")
 
-const response  = await model.invoke(message);
-message.push(response)
+    messages.push(new HumanMessage(userInput))
 
-console.log(`\x1b[31m AI: ${'\x1b[0m '}${response.content}\n`);
+  const response = await agent.invoke({ messages });
+    messages.push(response.messages[ response.messages.length - 1 ])
+
+    console.log(`\x1b[34m[AI]\x1b[0m ${response.messages[ response.messages.length - 1 ].content}`)
 }
-
